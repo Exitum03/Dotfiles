3ab28cc89748b95252d5a72e3961627a1bc3572e
Exitum03 <johnzeus.baay@ust-legazpi.edu.ph>
2022-03-27 19:48:21 +0800

    added exa aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+#
+# Alias
+#
+
+# exa
+alias ls="exa --color=always --icons --sort=name -l"
+alias lst="exa --color=always --icons --sort=name -lT"
+
